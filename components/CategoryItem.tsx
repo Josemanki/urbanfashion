c4f2484bf17969497255f3f0b7f083cf2d4dc27e
Jose Hernandez <josemanki@gmail.com>
2022-05-10 23:12:33 +0200

    Fixes categories not showing up on mobile, adds more height to images in them

--- components/CategoryItem.tsx
+++ components/CategoryItem.tsx
@@ -11,7 +11,7 @@ interface CategoryItemProps {
 
 export const CategoryItem: React.FC<CategoryItemProps> = ({ img, title, cat }) => {
   return (
-    <div className="flex-1 relative w-full h-[60vh]">
+    <div className="relative w-full h-[80vh] md:flex-1">
       <Image src={img} alt="" layout="fill" objectFit={'cover'} />
       <div className="md:opacity-0 transition-all duration-300 absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center hover:opacity-100">
         <div className="p-4 flex flex-col items-center justify-center bg-teal-500 skew-y-neg3">
